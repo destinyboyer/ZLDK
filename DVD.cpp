3f6ccb436d09dfa431cc1ad42aa101cbc231465c
Destiny Boyer <destiny@destinyboyer.com>
2017-03-13 16:32:44 -0700

    DVD class fully commented, tested, and functioning. No memory leaks.

--- DVD.cpp
+++ DVD.cpp
@@ -1,61 +1,86 @@
-#include "stdafx.h"
 #include "DVD.h"
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//								Constructors / Destructors										//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-////////////////////////////////////////////////////////////////////////////////
-//																			  //
-//							Constructors / Destructors						  //
-//																			  //
-////////////////////////////////////////////////////////////////////////////////
-
-
-
-DVD::DVD()
+DVD::DVD(void)
 {
-	this->date = new Date;
+	this->date = new Date();
 }
 
-
-DVD::~DVD()
+DVD::~DVD(void)
 {
 }
 
+DVD::DVD(const DVD& toCopy)
+{
+	this->date = new Date();
+	*this = toCopy;
+}
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//										Public Methods											//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////
-//																			  //
-//								Public Methods								  //
-//																			  //
-////////////////////////////////////////////////////////////////////////////////
+/*------------------------------------------------------------------------------------------------
 
+	Method takes in a file stream and populates the DVD's data members by calling the
+	setter methods. Setters are used as hepler methods for error checking. If any setter
+	method returns false, then setData will return false and the object should be deleted.
 
+	PRECONDITIONS:
+		- data must be formatted for DVD objects
+
+------------------------------------------------------------------------------------------------*/
 
 bool DVD::setData(istream & inFile)
 {
-	/* Implementation for Transaction - transfer when ready
-	// Our default DVD takes in "<title>, <release year>"
-	getline(inFile, this->title, ',');
-	int year;
-	inFile >> year;
-	this->date->setYear(year);
-	return true;
-	*/
-	string temp;
-	int year;
-	this->setType(inFile.get());
+	bool success = false;
+	bool typeSuccess, yearSuccess, directorSuccess, titleSuccess;
+	string temp = "";
+	int tempNum = 0;
+	typeSuccess = this->setType(inFile.get());
 	// This line removes all content up until the next comma
 	getline(inFile, temp, ',');
-	inFile >> this->inventory;
-	getline(inFile, temp, ',');
-	getline(inFile, this->director, ',');
-	getline(inFile, this->title, ',');
-	inFile >> year;
-	this->setYear(year);
-	// Do error checking here to decide return
-	this->display();
-	return true;
+	
+	inFile >> temp;						//gets inventory number
+	
+	if (tempNum > -1) {					//sets inventory if temp >= 0
+		this->adjustInventory(tempNum);
+	}
+
+	getline(inFile, temp, ',');			//fills temp with director's name
+
+	directorSuccess = this->setDirector(temp);
+
+	getline(inFile, temp, ',');			//fills temp wih the title
+
+	titleSuccess = this->setTitle(temp);
+
+	inFile >> tempNum;
+
+	yearSuccess = this->setYear(tempNum);
+
+	if (typeSuccess == true && directorSuccess == true &&
+		yearSuccess == true && titleSuccess == true)
+	{
+		success = true;
+	}
+	return success;
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Method prints all information of a DVD object to the console in an easy-to-read format.
+
+	NOTES:	Method cannot change any data members.
+
+------------------------------------------------------------------------------------------------*/
 
 void DVD::display(void) const
 {
@@ -63,81 +88,207 @@ void DVD::display(void) const
 		<< ", " << this->title << ", " << this->date->getYear() << endl;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//									Getters / Setters											//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*------------------------------------------------------------------------------------------------
 
-////////////////////////////////////////////////////////////////////////////////
-//								Getters / Setters							  //
-////////////////////////////////////////////////////////////////////////////////
+	Getter method for type.
 
+	NOTES:	Method cannot change any data members.
 
+------------------------------------------------------------------------------------------------*/
 
 char DVD::getType(void) const
 {
 	return this->type;
 }
 
+/*------------------------------------------------------------------------------------------------
+
+	Getter method for title.
+
+	NOTES:	Method cannot change any data members.
+
+------------------------------------------------------------------------------------------------*/
+
 string DVD::getTitle(void) const
 {
 	return this->title;
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Getter method for director.
+
+	NOTES:	Method cannot change any data members.
+
+------------------------------------------------------------------------------------------------*/
 
 string DVD::getDirector(void) const
 {
 	return this->director;
 }
 
+/*------------------------------------------------------------------------------------------------
+
+	Getter method for year.
+
+	NOTES:	Method cannot change any data members.
+
+------------------------------------------------------------------------------------------------*/
+
 int DVD::getYear(void) const
 {
 	return this->date->getYear();
 }
 
+/*------------------------------------------------------------------------------------------------
+
+	Setter method for type. Returns a bool indicating success.
+
+	PRECONDITIONS:
+		- input cannot be an empty string
+
+	POSTCONDITIONS:
+		- sets type equal to input
+
+------------------------------------------------------------------------------------------------*/
+
 bool DVD::setType(char input)
 {
-	this->type = input;
-	return false;
+	bool success = false;
+	if (input != ' ')
+	{
+		this->type = input;
+		success = true;
+	}
+	return success;
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Setter method for title. Returns a bool indicating success.
+
+	PRECONDITIONS:
+		- input cannot be an empty string
+
+	POSTCONDITIONS:
+		- sets title equal to input
+
+------------------------------------------------------------------------------------------------*/
 
 bool DVD::setTitle(string input)
 {
-	this->title = input;
-	return true;
+	bool success = false;
+	if (input != "")
+	{
+		this->title = input;
+		success = true;
+	}
+	return success;
 }
+/*------------------------------------------------------------------------------------------------
+
+	Setter method for director. Returns a bool indicating success.
+
+	PRECONDITIONS:
+		- input cannot be an empty string
+
+	POSTCONDITIONS:
+		- sets director equal to input
+
+------------------------------------------------------------------------------------------------*/
 
 bool DVD::setDirector(string input)
 {
-	this->director = input;
-	return true;
+	bool success = false;
+	if (input != "") {
+		this->director = input;
+		success = true;
+	}
+	return success;
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Setter method for year. Returns a bool indicating success. Uses date setter methods as
+	helper methods. Returns a bool indicating success.
+
+------------------------------------------------------------------------------------------------*/
 
 bool DVD::setYear(int input)
 {
 	return this->date->setYear(input);
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//									Overloaded Operators										//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*------------------------------------------------------------------------------------------------
 
-////////////////////////////////////////////////////////////////////////////////
-//									Operators								  //
-////////////////////////////////////////////////////////////////////////////////
+	Overloaded assignment operator. Creates a deep copy of the parameter Object.
 
+	PRECONDITIONS:
+		- parameter cannot be equal to calling object
 
+	POSTCONDITIONS:
+		- makes deep copy of parameter
 
-bool DVD::operator==(const DVD & toCompare) const
+------------------------------------------------------------------------------------------------*/
+
+DVD& DVD::operator=(const DVD& toCopy)
 {
-	return (this->date == toCompare.date && this->director == toCompare.getDirector() && this->title == toCompare.getTitle());
+	if (*this != toCopy)
+	{
+		delete this->date;
+		this->date = new Date();
+		this->date->setYear(toCopy.date->getYear());
+		this->type = toCopy.getType();
+		this->title = toCopy.getTitle();
+		this->director = toCopy.getDirector();
+		this->inventory = toCopy.getInventory();
+	}
+	return *this;
 }
 
-bool DVD::operator!=(const DVD & toCompare) const
+/*------------------------------------------------------------------------------------------------
+
+	Overloaded equality operator. Returns a bool indicating whether two DVD
+	objects are equal comparing all data members.
+
+	POSTCONDITIONS:
+		- returns a bool indicating whether two DVD objects are equal
+
+------------------------------------------------------------------------------------------------*/
+
+bool DVD::operator==(const DVD& toCompare) const
+{
+	return (this->title == toCompare.title &&
+		this->director == toCompare.director &&
+		this->type == toCompare.type &&
+		this->inventory == toCompare.inventory &&
+		this->date->getYear() == toCompare.date->getYear());
+}
+
+/*------------------------------------------------------------------------------------------------
+
+	Overloaded inequality operator. Returns a bool indicating whether two DVD
+	objects are equal comparing all data members. Uses the overloaded equality
+	method as a helper.
+
+	POSTCONDITIONS:
+		- returns a bool indicating whether two DVD objects are equal
+
+------------------------------------------------------------------------------------------------*/
+
+bool DVD::operator!=(const DVD& toCompare) const
 {
 	return !(*this == toCompare);
-}
-
-bool DVD::operator<(const DVD & toCompare) const
-{
-	return (this->date < toCompare.date);
-}
-
-bool DVD::operator>(const DVD & toCompare) const
-{
-	return (this->date > toCompare.date);
 }
--- dvd.h
+++ dvd.h
@@ -1,3 +1,21 @@
+/*-------------------------------------------------------------------------------------------------
+
+	Authors:		Boyer, Destiny
+					Selin, Zach
+
+	Created:		2/21/2017
+	Modified:		3/13/2017
+
+	This class represents a DVD object. Each object stores information about the title, director,
+	release year, and number of copies in inventory. Getter and setter methods are available
+	for all data members. This class has the functionality to take in a data stream with the
+	method setData() in order to populate its data members. Overloaded comparison operators
+	allow for comparing DVD objects, and the method adjustInventory() can be called to increase
+	or decrease the inventory amount. The method display() will neatly print a DVD object's
+	information to the console.
+
+-------------------------------------------------------------------------------------------------*/
+
 #pragma once
 #ifndef dvd_h
 #define dvd_h
@@ -10,10 +28,12 @@ using namespace std;
 
 class DVD : public Media
 {
+
 public:
+
 	DVD(void);
 	virtual ~DVD(void);
-	//DVD(DVD& toCopy);
+	DVD(const DVD& toCopy);
 
 	virtual bool setData(istream& inFile);
 	virtual void display(void) const;
@@ -28,16 +48,16 @@ public:
 	bool setDirector(string input);
 	bool setYear(int input);
 
+	virtual DVD& operator=(const DVD& toCopy);
 	virtual bool operator==(const DVD& toCompare) const;
 	virtual bool operator!=(const DVD& toCompare) const;
-	virtual bool operator<(const DVD& toCompare) const;
-	virtual bool operator>(const DVD& toCompare) const;
-	//virtual DVD& operator=(const DVD& toCopy) const;
 
 protected:
+
 	char type = ' ';
 	string title = "";
 	string director = "";
+
 };
 
 #endif
