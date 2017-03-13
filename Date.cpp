c4ef24df1967797d7af18d7514e6dd4b09042d78
Destiny Boyer <destiny@destinyboyer.com>
2017-03-13 14:32:48 -0700

    Date class fully commented and tested.

--- Date.cpp
+++ Date.cpp
@@ -1,81 +1,246 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "Date.h"
 
 
 
-////////////////////////////////////////////////////////////////////////////////
-//																			  //
-//							Constructors / Destructors						  //
-//																			  //
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//								Constructors / Destructors										//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-Date::Date()
+Date::Date(void)
 {
 }
 
 
-Date::~Date()
+Date::~Date(void)
 {
 }
 
 
 
-////////////////////////////////////////////////////////////////////////////////
-//																			  //
-//								Public Methods								  //
-//																			  //
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//										Public Methods											//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*------------------------------------------------------------------------------------------------
+
+	Method takes in a file stream and  populates a Date object's data members by calling setters
+	as helper methods for error checking. If any of the setters returns false then setData() will
+	return false and the object should be deleted.
+
+------------------------------------------------------------------------------------------------*/
+
+bool Date::setData(istream& inFile)
+{
+	bool success = false;
+	bool daySuccess, monthSuccess, yearSuccess;
+
+	int tempDay = 0;
+	int tempMonth = 0;
+	int tempYear = 0;
+
+	inFile >> tempDay;
+
+	if (tempDay != 0)
+	{
+		inFile >> tempMonth >> tempYear;
+		daySuccess = this->setDay(tempDay);
+		monthSuccess = this->setMonth(tempMonth);
+		yearSuccess = this->setYear(tempYear);
+
+		if (daySuccess == true && monthSuccess == true
+			&& yearSuccess == true)
+		{
+			success == true;
+		}
+	}
+	return success;
+}
+
+/*------------------------------------------------------------------------------------------------
+
+	Method displays the date in month, date, year, format.
+
+	POSTCONDITIONS:
+	 - returns a string displaying the date in month, date, year format.
+
+------------------------------------------------------------------------------------------------*/
+
+string Date::display(void) const
+{
+	string retVal = "";
+
+	stringstream convert;
+
+	convert << getMonth() << "/" << getDay() << "/" << getYear();
+	retVal = convert.str();
+
+	return retVal;
+}
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//									Getters / Setters											//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////
-//								Getters / Setters							  //
-////////////////////////////////////////////////////////////////////////////////
+/*------------------------------------------------------------------------------------------------
 
+	Getter method for the day.
 
+	POSTCONDITIONS:
+		- returns the day
+
+------------------------------------------------------------------------------------------------*/
 
 int Date::getDay(void) const
 {
 	return this->day;
 }
 
+/*------------------------------------------------------------------------------------------------
+
+	Getter method for the month.
+
+	POSTCONDITIONS:
+		- returns the month
+
+------------------------------------------------------------------------------------------------*/
+
 int Date::getMonth(void) const
 {
 	return this->month;
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Getter method for the year.
+
+	POSTCONDITIONS:
+		- returns the year
+
+------------------------------------------------------------------------------------------------*/
 
 int Date::getYear(void) const
 {
 	return this->year;
 }
 
+/*------------------------------------------------------------------------------------------------
+
+	Setter method for day. Returns a bool indicating success.
+
+	PRECONDITIONS:
+		- input must be greater than 0
+		- input must be less that 32
+
+	POSTCONDITIONS:
+		- sets day to input
+
+------------------------------------------------------------------------------------------------*/
+
 bool Date::setDay(int input)
 {
-	this->day = input;
-	return true;
+	bool success = false;
+
+	if (input > 0 && input < 32)	//checks to ensure that the date
+	{								//is in between 1 and 31 (inclusive)
+		this->day = input;
+		success = true;
+	}
+
+	return success;
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Setter method for month. Returns a bool indicating success.
+
+	PRECONDITIONS:
+		- input must be greater than 0
+		- input must be less that 13
+
+	POSTCONDITIONS:
+		- sets month to input
+
+------------------------------------------------------------------------------------------------*/
 
 bool Date::setMonth(int input)
 {
-	this->month = input;
-	return true;
+	bool success = false;
+
+	if (input > 0 && input < 13)	//checks to ensure that the month
+	{								//is in between 1 and 12 (inclusive)
+		this->month = input;
+		success = true;
+	}
+
+	return success;
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Setter method for year. Returns a bool indicating success.
+
+	PRECONDITIONS:
+		- input must be greater than 999
+		- input must be less that 10000
+
+	POSTCONDITIONS:
+		- sets year to input
+
+------------------------------------------------------------------------------------------------*/ 
 
 bool Date::setYear(int input)
 {
-	this->year = input;
-	return true;
+	bool success = false;
+
+	if (input > 999 && input < 10000)	//checks to ensure that the year
+	{									//is a 4 digit number
+		this->year = input;
+		success = true;
+	}
+
+	return success;
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//																								//
+//									Overloaded Operators										//
+//																								//
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*------------------------------------------------------------------------------------------------
 
-////////////////////////////////////////////////////////////////////////////////
-//									Operators								  //
-////////////////////////////////////////////////////////////////////////////////
+	Creates deep copy of a date object.
 
+	POSTCONDITIONS:
+	- creates deep copy of a Date object
 
+------------------------------------------------------------------------------------------------*/
+
+Date& Date::operator=(const Date& toCopy)
+{
+	this->day = toCopy.getDay();
+	this->month = toCopy.getMonth();
+	this->year = toCopy.getYear();
+
+	return *this;
+}
+
+/*------------------------------------------------------------------------------------------------
+
+	Overloaded equality operator. Returns if the month, date, and year of two Date
+	objects are equal.
+
+------------------------------------------------------------------------------------------------*/
 
 bool Date::operator==(const Date & toCompare) const
 {
@@ -84,16 +249,36 @@ bool Date::operator==(const Date & toCompare) const
 		this->getYear() == toCompare.getYear());
 }
 
+/*------------------------------------------------------------------------------------------------
+
+	Overloaded equality operator. Returns if the month, date, and year of two Date
+	objects are not equal. Uses the overloaded equality operator as a helper.
+
+------------------------------------------------------------------------------------------------*/
+
 bool Date::operator!=(const Date & toCompare) const
 {
 	return !(*this == toCompare);
 }
+/*------------------------------------------------------------------------------------------------
+
+	Overloaded equality operator. Returns true if the calling object comes before the parameter
+	object.
+
+------------------------------------------------------------------------------------------------*/
 
 bool Date::operator<(const Date & toCompare) const
 {
 	 return ((this->getYear() * 10000 + this->getMonth() * 100 + this->getDay())
-	 < (toCompare.getYear()*10000 + toCompare.getMonth()*100 + this->getDay()));
+	 < (toCompare.getYear() * 10000 + toCompare.getMonth() * 100 + this->getDay()));
 }
+
+/*------------------------------------------------------------------------------------------------
+
+	Overloaded equality operator. Returns true if the calling object comes after the parameter
+	object.
+
+------------------------------------------------------------------------------------------------*/
 
 bool Date::operator>(const Date & toCompare) const
 {
--- date.h
+++ date.h
@@ -1,17 +1,34 @@
+/*-------------------------------------------------------------------------------------------------
+
+	Authors:		Boyer, Destiny
+					Selin, Zach
+
+	Created:		3/1/2017
+	Modified:		3/13/2017
+
+	Date class represents a date with data members corresponding to the day, month, and year.
+	Class has functionality to take in a data stream in order to populate data members.
+
+-------------------------------------------------------------------------------------------------*/
+
+
 #pragma once
 #ifndef date_h
 #define date_h
 
 #include <istream>
+#include <sstream>
 #include <string>
 
 using namespace std;
 
 class Date
 {
+
 public:
-	Date();
-	~Date();
+
+	Date(void);
+	~Date(void);
 
 	bool setData(istream& inFile);
 
@@ -23,6 +40,8 @@ public:
 	bool setMonth(int input);
 	bool setYear(int input);
 
+	string display(void) const;
+
 	Date& operator=(const Date& toCopy);
 	bool operator==(const Date& toCompare) const;
 	bool operator!=(const Date& toCompare) const;
@@ -30,10 +49,12 @@ public:
 	bool operator>(const Date& toCompare) const;
 
 private:
-	// default January 1st, 2000
-	int day = 1;
-	int month = 1;
-	int year = 2000;
+
+	// default date of 0-0-0000
+	int day = 0;
+	int month = 0;
+	int year = 0000;
+
 };
 
 #endif
